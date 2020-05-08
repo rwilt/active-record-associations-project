a338ec9c5976622f02d96326b7bf9f74fe990d42
rwilt <rosie.wilt@gmail.com>
2020-05-08 09:09:37 -0400

    update

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
     has_many :books
-    has_many :authors, through: :books
+    # has_many :authors, through: :books
     #books is the join class for authors and categories
 end
